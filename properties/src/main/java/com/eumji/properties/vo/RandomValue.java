253325e7988988a82aa5692b22912057b649239b
eumji025 <zombie_goo@yeah.net>
2017-03-26 12:56:50 +0800

    合并配置文件

--- properties/src/main/java/com/eumji/properties/vo/RandomValue.java
+++ properties/src/main/java/com/eumji/properties/vo/RandomValue.java
@@ -1,6 +1,7 @@
 package com.eumji.properties.vo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
