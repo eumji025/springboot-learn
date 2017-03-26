30d9c34eb160cc704556c4761cc8664baf099a2a
eumji025 <zombie_goo@yeah.net>
2017-03-26 14:05:21 +0800

    跳转静态页面

---
+++ indexPage/README.md
@@ -0,0 +1,2 @@
+## indexPage
+此demo是用来跳转静态页面用 的
---
+++ indexPage/src/main/java/com/eumji/indexPage/ApplicationInit.java
@@ -0,0 +1,22 @@
+package com.eumji.indexPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * com.eumji.indexPage.ApplicationInit.java
+ *
+ * @desc 不积跬步无以至千里, 不积小流无以至千里
+ * @author:EumJi
+ * @year: 2017
+ * @month: 03
+ * @day: 26
+ * @time: 2017/3/26
+ */
+@SpringBootApplication
+public class ApplicationInit {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationInit.class,args);
+    }
+}
