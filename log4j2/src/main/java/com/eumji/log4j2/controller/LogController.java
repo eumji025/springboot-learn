af3403b89b7271a1942abf5e4ff7fa81142b4d84
eumji025 <zombie_goo@yeah.net>
2017-03-28 11:16:56 +0800

    log4j2demo

---
+++ log4j2/src/main/java/com/eumji/log4j2/controller/LogController.java
@@ -0,0 +1,25 @@
+package com.eumji.log4j2.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Do
+ * @package com.eumji.log4j2.controller
+ * @name LogController
+ * @date 2017/3/28
+ * @time 11:03
+ */
+@RestController
+public class LogController {
+    Logger log = LoggerFactory.getLogger(LogController.class);
+    @RequestMapping("log")
+    public String getLog(){
+        log.info("this is info log");
+        log.debug("this is debug log");
+        log.error("this is error log");
+        return "ok";
+    }
+}
