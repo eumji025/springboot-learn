95d3ba7066620ee5c4bbd432c24f4279b4b114ff
eumji025 <zombie_goo@yeah.net>
2017-04-01 07:52:29 +0800

    使用swagger构建restful api

---
+++ swagger/src/main/java/com/eumji/swagger/Application.java
@@ -0,0 +1,21 @@
+package com.eumji.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * com.eumji.swagger.Application.java
+ *
+ * @desc 不积跬步无以至千里, 不积小流无以至千里
+ * @author:EumJi
+ * @year: 2017
+ * @month: 03
+ * @day: 31
+ * @time: 2017/3/31
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ swagger/src/main/java/com/eumji/swagger/vo/User.java
@@ -0,0 +1,58 @@
+package com.eumji.swagger.vo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * com.eumji.swagger.vo.User.java
+ *
+ * @desc 不积跬步无以至千里, 不积小流无以至千里
+ * @author:EumJi
+ * @year: 2017
+ * @month: 04
+ * @day: 01
+ * @time: 2017/4/1
+ */
+public class User {
+
+    private Long id;
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
