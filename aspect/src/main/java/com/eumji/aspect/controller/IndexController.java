46553f4df4a9d1bc8f455df5ca523dbeb16e82cc
eumji025 <zombie_goo@yeah.net>
2017-04-01 09:52:19 +0800

    去掉无用注释

--- aspect/src/main/java/com/eumji/aspect/controller/IndexController.java
+++ aspect/src/main/java/com/eumji/aspect/controller/IndexController.java
@@ -1,7 +1,5 @@
 package com.eumji.aspect.controller;
 
-import com.sun.org.apache.regexp.internal.RE;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
