bdacac5bd193cd42a6dd6557a7a3d9d6a41e44d4
eumji025 <eumji025@gmail.com>
2017-11-25 19:35:55 +0800

    update aspect demo

---
+++ spring-boot-aspect-demo/src/main/java/com/eumji/aspect/repository/LogRepository.java
@@ -0,0 +1,17 @@
+package com.eumji.aspect.repository;
+
+import com.eumji.aspect.vo.LogInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * com.eumji.aspect.repository.LogRepository.java
+ *
+ * @desc 不积跬步无以至千里, 不积小流无以至千里
+ * @author:EumJi
+ * @year: 2017
+ * @month: 04
+ * @day: 01
+ * @time: 2017/4/1
+ */
+public interface LogRepository extends JpaRepository<LogInfo,Long> {
+}
