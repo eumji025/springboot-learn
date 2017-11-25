7b84e93aea5660f74a174bec0e32774018481a02
eumji025 <eumji025@gmail.com>
2017-11-25 21:47:55 +0800

    jdbc的方式操作数据库

---
+++ spring-boot-jdbc-demo/src/test/java/com/eumji/jdbcMysql/JdbcTemplateTest.java
@@ -0,0 +1,52 @@
+package com.eumji.jdbcMysql;
+
+import com.eumji.jdbcMysql.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 简单的增删改查测试
+ * @author Do
+ * @package eumji.eumji.jdbcMysql
+ * @name JdbcTemplateTest
+ * @date 2017/3/27
+ * @time 17:31
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = JdbcApplication.class)
+public class JdbcTemplateTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void addUser(){
+        userService.addUser("lisi",30);
+    }
+
+    @Test
+    public void getAllUser(){
+        List<Map<String ,Object>> list = userService.getAllUser();
+        for (Map<String,Object> map:
+             list) {
+            System.out.println("userName:"+map.get("userName"));
+            System.out.println("age:"+map.get("age"));
+        }
+    }
+
+    @Test
+    public void updateUser(){
+        userService.updateUser();
+    }
+
+    @Test
+    public void deleteUserByName(){
+        userService.deleteUserByName("zhangsan");
+    }
+
+}
