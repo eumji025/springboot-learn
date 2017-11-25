08a8c935748ba879304bd5b8f3bcfc04b2bbca9f
eumji025 <eumji025@gmail.com>
2017-11-25 18:26:10 +0800

    添加actuator的demo

---
+++ actuator/src/main/java/com/eumji/actuator/ActuatorApplication.java
@@ -0,0 +1,20 @@
+package com.eumji.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @email eumji025@gmail.com
+ * @author:EumJi
+ * @date: 2017/11/25
+ * @time: 18:20
+ */
+@SpringBootApplication
+public class ActuatorApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class,args);
+    }
+
+}
